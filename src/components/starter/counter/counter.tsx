df9c441fb40a5cf93763c9bc6cc0a50e5dfa37fb
cookie_gg <cookie.gg.dev@gmail.com>
2023-06-18 12:07:26 +0900

    Try that which is good for object state, useSignal or useStore

--- src/components/starter/counter/counter.tsx
+++ src/components/starter/counter/counter.tsx
@@ -4,23 +4,37 @@ import Gauge from '../gauge';
 
 export const Counter = component$(() => {
   const count = useSignal(70);
+  const state = useSignal({ count: 70 });
 
   const setCount = $((newValue: number) => {
     if (newValue < 0 || newValue > 100) {
       return;
     }
+    state.value.count = newValue; //! This expression is not working
+    // state.value = { count: newValue }; //* This expression is working
     count.value = newValue;
   });
 
   return (
-    <div class={styles['counter-wrapper']}>
-      <button class="button-dark button-small" onClick$={() => setCount(count.value - 1)}>
-        -
-      </button>
-      <Gauge value={count.value} />
-      <button class="button-dark button-small" onClick$={() => setCount(count.value + 1)}>
-        +
-      </button>
-    </div>
+    <>
+      <div class={counterWrapper().toString()}>
+        <button class="button-dark button-small" onClick$={[print, $(() => setCount(count.value - 1))]}>
+          - (Multiple Events)
+        </button>
+        <Gauge value={count.value} />
+        <button class="button-dark button-small" onClick$={() => setCount(count.value + 1)}>
+          +
+        </button>
+      </div>
+      <div class={styles['counter-wrapper']}>
+        <button class="button-dark button-small" onClick$={() => setCount(state.value.count - 1)}>
+          -
+        </button>
+        <Gauge value={state.value.count} />
+        <button class="button-dark button-small" onClick$={() => setCount(state.value.count + 1)}>
+          +
+        </button>
+      </div>
+    </>
   );
 });

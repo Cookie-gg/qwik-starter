3d8bebd17957c18e4af8aeb3e11230927e1d5899
cookie_gg <cookie.gg.dev@gmail.com>
2023-06-18 12:10:36 +0900

    Use 'useNavigate' instead of anchor link

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,15 +1,17 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikLogo } from '../icons/qwik';
 import styles from './header.module.css';
+import { useNavigate } from '@builder.io/qwik-city';
 
 export default component$(() => {
+  const nav = useNavigate();
   return (
     <header class={styles.header}>
       <div class={['container', styles.wrapper]}>
-        <div class={styles.logo}>
-          <a href="/" title="qwik">
-            <QwikLogo height={50} width={143} />
-          </a>
+        <div class={styles.logo} onClick$={() => nav('/')}>
+          {/* <a href="/" title="qwik"> */}
+          <QwikLogo height={50} width={143} />
+          {/* </a> */}
         </div>
         <ul>
           <li>
